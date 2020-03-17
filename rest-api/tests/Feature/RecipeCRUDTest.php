6b447e161d0c1c2b1ea4f65a52ffe0ccdd7a8ccd
Paolo <scottypipz@gmail.com>
2020-03-17 21:33:15 +0800

    Stash

--- rest-api/tests/Feature/RecipeCRUDTest.php
+++ rest-api/tests/Feature/RecipeCRUDTest.php
@@ -8,15 +8,22 @@ use Tests\TestCase;
 
 class RecipeCRUDTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    public function testStore__validateEmptyFields(): void
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $faker = \Faker\Factory::create();
+        $payload = [
+            'name' => 'TestStoreProcedure',
+            'description' => $faker->sentence(),
+            'duration_from_minute' => 20,
+            'duration_to_minute' => 25,
+        ];
+        $this->json('POST', '/api/procedures', $payload)
+            ->assertStatus(201)
+            ->assertJsonStructure([
+                'name',
+                'description',
+                'duration_from_minute',
+                'duration_to_minute',
+            ]);
     }
 }
