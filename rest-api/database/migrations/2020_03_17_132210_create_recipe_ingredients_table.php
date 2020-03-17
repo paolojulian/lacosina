99625994811f31faff34685338a0d64ffde2544c
Paolo <scottypipz@gmail.com>
2020-03-17 21:32:33 +0800

    Added migrations

---
+++ rest-api/database/migrations/2020_03_17_132210_create_recipe_ingredients_table.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRecipeIngredientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $this->down();
+        Schema::create('recipe_ingredients', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('measurement');
+            $table->string('optional_name')
+                ->nullable()
+                ->default(null)
+                ->max(50);
+                
+            $table->text('description')->max(1000);
+
+            $table->string('image')
+                ->nullable()
+                ->default(null);
+
+            $table->bigInteger('recipe_id')
+                ->unsigned();
+            $table->foreign('recipe_id')
+                ->references('id')
+                ->on('recipes')
+                ->onDelete('cascade');
+            
+            $table->bigInteger('ingredient_id')
+                ->unsigned();
+            $table->foreign('ingredient_id')
+                ->references('id')
+                ->on('ingredients')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+
+            $table->index('recipe_id');
+            $table->unique(['recipe_id', 'ingredient_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipe_ingredients');
+    }
+}
