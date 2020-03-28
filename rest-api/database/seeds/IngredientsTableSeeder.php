656c826af69288ad9a752e8b3bf29fc618893a95
scottypipz <scottypipz@gmail.com>
2020-03-28 18:51:23 +0800

    Seeder

--- rest-api/database/seeds/IngredientsTableSeeder.php
+++ rest-api/database/seeds/IngredientsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Ingredient;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,7 +14,7 @@ class IngredientsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('ingredients')->truncate();
+        Ingredient::query()->delete();
         $faker = \Faker\Factory::create();
         $ingredients = [
             [
@@ -21,16 +22,46 @@ class IngredientsTableSeeder extends Seeder
                 'description' => $faker->sentence(),
                 'category' => 'Base',
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
             [
                 'name' => 'Pepper',
                 'description' => $faker->sentence(),
                 'category' => 'Base',
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Chicken',
+                'description' => $faker->sentence(),
+                'category' => 'Meat',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Pork',
+                'description' => $faker->sentence(),
+                'category' => 'Base',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Beef',
+                'description' => $faker->sentence(),
+                'category' => 'Base',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
         ];
+        for ($i = 0, $len = 50; $i < $len; $i += 1) {
+            $ingredients[] = [
+                'name' => $faker->colorName(),
+                'description' => $faker->sentence(),
+                'category' => 'Base',
+                'created_at' => Carbon::now()->addMinute($i + 1)->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->addMinute($i + 1)->format('Y-m-d H:i:s'),
+            ];
+        }
         DB::table('ingredients')->insert($ingredients);
     }
 }
--- rest-api/database/seeds/ProceduresTableSeeder.php
+++ rest-api/database/seeds/ProceduresTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Procedure;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,28 +14,57 @@ class ProceduresTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('procedures')->truncate();
+        Procedure::query()->delete();
         $faker = \Faker\Factory::create();
-        $data = [
+        $procedures = [
             [
                 'name' => 'Sautee',
                 'description' => $faker->sentence(),
-                'image' => '',
+                'image' => $faker->imageUrl(),
                 'duration_from_minute' => 3,
                 'duration_to_minute' => 5,
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
                 'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
             ],
             [
-                'name' => 'Deep Fry',
+                'name' => 'Stir Fry',
                 'description' => $faker->sentence(),
-                'image' => '',
+                'image' => $faker->imageUrl(),
                 'duration_from_minute' => 20,
                 'duration_to_minute' => 30,
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Reduce',
+                'description' => $faker->sentence(),
+                'image' => $faker->imageUrl(),
+                'duration_from_minute' => 20,
+                'duration_to_minute' => 30,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Deep Fry',
+                'description' => $faker->sentence(),
+                'image' => $faker->imageUrl(),
+                'duration_from_minute' => 20,
+                'duration_to_minute' => 30,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
             ],
         ];
-        DB::table('procedures')->insert($data);
+        for ($i = 0, $len = 50; $i < $len; $i += 1) {
+            $procedures[] = [
+                'name' => $faker->colorName(),
+                'description' => $faker->sentence(),
+                'image' => $faker->imageUrl(),
+                'duration_from_minute' => rand(1, 20),
+                'duration_to_minute' => rand(20, 40),
+                'created_at' => Carbon::now()->addMinute($i + 1)->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->addMinute($i + 1)->format('Y-m-d H:i:s'),
+            ];
+        }
+        DB::table('procedures')->insert($procedures);
     }
 }
